dca3fb15a4b1c3dff82b12774fbd157703ae99df
Benoît Hubert <benoithubert@gmail.com>
2021-02-27 08:07:44 +0100

    Handle max level of recursion

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ A tool for this: https://benoithubert.net/indexer-manuellement-ses-fichiers-sur-
 ## Credits
 
 * Alexander Peslyak, better known as Solar Designer <solar at openwall.com> for [md5 source code in C](http://openwall.info/wiki/people/solar/software/public-domain-source-code/md5)
-* [Michael Kerrisk](http://man7.org/mtk/index.html) for providing base code for inotify use (some of his code, found [here](http://man7.org/tlpi/code/index.html), is in folder `inotify/` as of 
+* [Michael Kerrisk](http://man7.org/mtk/index.html) for providing base code for inotify use (some of his code, found [here](http://man7.org/tlpi/code/index.html), is in folder `inotify/` as of
 first commits).
 * mloydm for providing [this code on SO](http://stackoverflow.com/questions/8436841/how-to-recursively-list-directories-in-c-on-linux)
 * Graham Poulter for providing [this code](http://snipplr.com/view/4025/mp3-checksum-in-id3-tag/) that I translated into C
@@ -31,3 +31,24 @@ first commits).
 
 * Generate full md5 anyway in addition to audio hash for mp3 files
 * Change response from server: add success flag in all cases, put output file in "file" obj for example
+
+## Benchmarking
+
+### Scan in WSL
+
+136348 dirs / 802393 files: 63 sec. then 19 only, then 17! (with printf'ing all files along the way)
+
+### Scan in C:
+
+```
+benoit@DESKTOP-TMB3JDP:~/Code/backup-tool/recurse$ ./ldir /mnt/c/Users/Ben/Code/
+4716 dirs scanned
+13397 files found
+11.000000 seconds elapsed
+benoit@DESKTOP-TMB3JDP:~/Code/backup-tool/recurse$ ./ldir /mnt/c/Users/Ben/Wild/
+28527 dirs scanned
+131563 files found
+69.000000 seconds elapsed
+```
+
+It's so damn slow.
--- recurse/ldir.c
+++ recurse/ldir.c
@@ -6,11 +6,13 @@
 #include <stdbool.h>
 #include <string.h>
 #include <sys/stat.h>
+#include <time.h> // http://www.cplusplus.com/reference/ctime/difftime/
 
 #define FILE_MIN 1048576
 
 int num_opened = 0;
 int num_files = 0;
+int num_dirs = 0;
 const char *accepted_exts[] = {"mp3","m4a","flac"};
 
 char *get_filename_ext(const char *filename) {
@@ -21,18 +23,20 @@ char *get_filename_ext(const char *filename) {
 
 int search_array(const char **arr, char *str, int len) {
     int i;
+    if (!strlen(str)) return 0;
     for (i = 0; i < len; i++) {
         if (!strncmp(arr[i], str, strlen(str))) return 1;
     }
     return 0;
 }
 
-void list_dirs(const char *name, int level)
+void list_dirs(const char *name, int level, int max_level)
 {
     DIR *dir;
     struct dirent *entry;
     char *ext;
 
+    if ((max_level > -1) && (level == max_level)) return;
 
     if (!(dir = opendir(name)))
         return;
@@ -48,15 +52,16 @@ void list_dirs(const char *name, int level)
             path[len] = 0;
             if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
                 continue;
-            printf("      %*s[%s]\n", level*2, "", entry->d_name);
-            list_dirs(path, level + 1);
+            num_dirs++;
+            // printf("      %*s[%s]\n", level*2, "", entry->d_name);
+            list_dirs(path, level + 1, max_level);
         }
         else {
             ext = get_filename_ext( entry->d_name );
-            if( search_array( accepted_exts, ext, 3 ) != -1 ) {
-                num_files++;
-                printf("%5d %*s- %s\n", num_files, level*2, "", entry->d_name);
-            } 
+            // if( search_array( accepted_exts, ext, 3 ) ) {
+            num_files++;
+            // printf("%5d %*s- %s\n", num_files, level*2, "", entry->d_name);
+            // }
         }
     } while ((entry = readdir(dir)));
     closedir(dir);
@@ -64,12 +69,23 @@ void list_dirs(const char *name, int level)
 
 int main(int argc, char **argv)
 {
+    time_t start;
+    time_t end;
+    double seconds;
+
     if(argc < 2) {
         printf("Not enough arguments:\n  ldir <dir>\n\n");
         exit(-1);
     }
 
-    list_dirs(argv[1], 0);
+    start = time(NULL);
+    list_dirs(argv[1], 0, 2);
+    end = time(NULL);
+
+    seconds = difftime(end, start);
+    printf("%d dirs scanned\n", num_dirs);
+    printf("%d files found\n", num_files);
+    printf("%f seconds elapsed\n", seconds);
     return 0;
 }
 
