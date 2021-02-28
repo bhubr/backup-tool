e0e3359ad94d8bb8c296d040512332dffc0cedf0
Benoît Hubert <benoithubert@gmail.com>
2021-02-28 04:43:02 +0100

    babel stuff

--- express-server/client/stm.js
+++ express-server/client/stm.js
@@ -1,5 +1,38 @@
 let n = 0
 
+class Rectangle {
+  static stack = []
+
+  constructor (w, h) {
+    this.w = w
+    this.h = h
+    this.direction = this.h < this.w ? 'row' : 'column'
+    this.children = []
+  }
+
+  layoutrow (row) {
+    console.log('>>> layout', row)
+    const rowArea = row.reduce((c, r) => c + r, 0)
+    const otherSide = rowArea / this.width()
+    let w
+    let h
+    if (this.direction === 'row') {
+      h = '100%'
+      w = `${otherSide * 100 / this.w}%`
+    } else {
+      w = '100%'
+      h = `${otherSide * 100 / this.h}%`
+    }
+    console.log('<<< layout', otherSide, w, h)
+  }
+
+  width () {
+    return Math.min(this.w, this.h)
+  }
+}
+
+const rect = new Rectangle(6, 4)
+
 function worst (R, w) {
   const s = R.reduce((c, r) => c + r, 0)
   const sq = s ** 2
@@ -8,14 +41,6 @@ function worst (R, w) {
   const rMax = Math.max(...R)
   console.log(Math.max(wq * rMax / sq, sq / (wq * rMin)))
   return Math.max(wq * rMax / sq, sq / (wq * rMin))
-}
-
-function layoutrow (row) {
-  console.log('should layout', row)
-}
-
-function width () {
-
 }
 
 function squarify (children, row, w) {
@@ -29,9 +54,9 @@ function squarify (children, row, w) {
   if ((r1 === 0) || (r1 > r2)) {
     squarify(children.slice(1), [...row, c], w)
   } else {
-    layoutrow(row)
-    squarify(children, [], width())
+    rect.layoutrow(row)
+    squarify(children, [], rect.width())
   }
 }
 
-squarify([6, 6, 4, 3, 2, 2, 1], [], 4)
+squarify([6, 6, 4, 3, 2, 2, 1], [], rect.width())
